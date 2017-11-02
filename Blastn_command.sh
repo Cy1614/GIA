7e0872e38811426049abe346801597cc8d04f60d
Cy1614 <albertyu97@gmail.com>
2017-11-02 16:56:30 +0000

    Update Blastn_command.sh

--- Blastn_command.sh
+++ Blastn_command.sh
@@ -14,8 +14,9 @@ blastn -query $query -subject $target -task blastn -outfmt "6" -out blastn_moj.t
 #------------------------------ the following clot is for Albert -------------------------------------------------------#
 export PATH="$PATH:/local/data/public/genome_informatics_2017/programs/ncbi-blast-2.5.0+/bin"
 cd /home/cy302/Genome_Informatics/Assignment_2/alignment
-query="/home/cy302/Genome_Informatics/Assignment_2/dmel-all-transcript-r6.18.fasta"
+query_c="/home/cy302/Genome_Informatics/Assignment_2/dmel-all-transcript-r6.18.fasta"
+query_l="/home/cy302/Genome_Informatics/Assignment_2/dmel-all-translation-r6.18.fasta"
 target="/home/cy302/Genome_Informatics/Assignment_2/dvir-all-chromosome-r1.06.fasta"
-tblastn -query $query -subject $target -outfmt "6" -out tbalstn_dvir.txt -evalue 1e-3
-blastn -query $query -subject $target -outfmt "6" -out balstn_dvir.txt -evalue 1e-3
+blastn -query $query_l -subject $target -outfmt "6" -out balstn_dvir.txt -evalue 1e-3
+tblastn -query $query_c -subject $target -outfmt "6" -out tbalstn_dvir.txt -evalue 1e-3
 #-----------------------------------------------------------------------------------------------------------------------#
