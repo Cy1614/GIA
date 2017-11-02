93a09667f2d8f4cc064715a96139b46f6303807d
Cy1614 <albertyu97@gmail.com>
2017-11-02 18:05:17 +0000

    Update Blastn_command.sh

--- Blastn_command.sh
+++ Blastn_command.sh
@@ -5,7 +5,7 @@ target="/local/data/public/ctl43/gia2/db/dmoj-all-chromosome-r1.04.fasta"
 
 query_tblastn="/local/data/public/ctl43/gia2/query/dmel-all-translation-r6.18.fasta"
 query_blastn="/local/data/public/ctl43/gia2/query/dmel-all-transcript-r6.18.fasta"
-tblastn -query $query_blastn -subject $target -outfmt "6" -out tblastn_moj.txt -evalue 1e-3
+tblastn -query $query_tblastn -subject $target -outfmt "6" -out tblastn_moj.txt -evalue 1e-3
 blastn -query $query_blastn -subject $target -task blastn -outfmt "6" -out blastn_moj.txt -evalue 1e-3
 
 
