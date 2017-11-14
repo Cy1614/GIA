05d5925ac882f190cb272dd580c95acc7baf92df
Cy1614 <albertyu97@gmail.com>
2017-11-14 23:31:47 +0000

    Update blastn2genscan.R

--- blastn2genscan.R
+++ blastn2genscan.R
@@ -5,9 +5,17 @@ df.dmoj.tblastn <- fread('tblastn_dmoj.txt')
 df.dvir.blastn <- fread('blastn_dvir.txt')
 df.dvir.tblastn <- fread('tblastn_dvir.txt')
 df.dgri.blastn <- fread('blastn_dgri.txt')
-df.dgri.blastn <- fread('tblastn_dgri.txt')
+df.dgri.tblastn <- fread('tblastn_dgri.txt')
 
 names(df.dmoj.blastn) <- c('Query/Gene_ID', 'Target/Scaffold_ID', 'Percent_Identity', 
+                           'Alignment_Length', 'Num_Mismathces', 'Num_Gap', 
+                           'Start_Query_Pos', 'End_Query_Pos', 'Start_Target_Pos', 
+                           'End_Target_pos', 'E_val', 'Bit_score')
+names(df.dvir.blastn) <- c('Query/Gene_ID', 'Target/Scaffold_ID', 'Percent_Identity', 
+                           'Alignment_Length', 'Num_Mismathces', 'Num_Gap', 
+                           'Start_Query_Pos', 'End_Query_Pos', 'Start_Target_Pos', 
+                           'End_Target_pos', 'E_val', 'Bit_score')
+names(df.dgri.blastn) <- c('Query/Gene_ID', 'Target/Scaffold_ID', 'Percent_Identity', 
                            'Alignment_Length', 'Num_Mismathces', 'Num_Gap', 
                            'Start_Query_Pos', 'End_Query_Pos', 'Start_Target_Pos', 
                            'End_Target_pos', 'E_val', 'Bit_score')
@@ -32,14 +40,41 @@ transfer.bl.gc <- function(df){
   s[which(s==0)] <- '-'
   s[which(s==1)] <- '+'
   df$S<-s
-  write.table(df, 'blastn_dmoj_converted.txt',col.names=T,row.names=F,quote=F, sep='\t')
-  nrow(df)
+  df <- data.frame(cbind(df$Gene, df$S, df$`Target/Scaffold_ID`, df$Alignment_Length, 
+                         df$Start_Target_Pos, df$End_Target_pos, df$E_val, df$Bit_score))
   return(df)
 }
+
+e_val_cutoff.dmoj <- calculate_cutoff(df.dmoj.blastn$E_val)
+e_val_cutoff.dvir <- calculate_cutoff(df.dvir.blastn$E_val)
+e_val_cutoff.dgri <- calculate_cutoff(df.dgri.blastn$E_val)
+
+df.dmoj.blastn <- df.dmoj.blastn[df.dmoj.blastn$E_val < e_val_cutoff.dmoj]
+df.dvir.blastn <- df.dvir.blastn[df.dvir.blastn$E_val < e_val_cutoff.dvir]
+df.dgri.blastn <- df.dgri.blastn[df.dgri.blastn$E_val < e_val_cutoff.dgri]
+
+df.dmoj.blastn <- transfer.bl.gc(df.dmoj.blastn)
+df.dvir.blastn <- transfer.bl.gc(df.dvir.blastn)
+df.dgri.blastn <- transfer.bl.gc(df.dgri.blastn)
+
+colnames(df.dmoj.blastn) <- c("Gene_ID","S", 'Scaffold_ID', 'Alignment_Length', 'Begin', 
+                              'End', 'E_val', 'Bit_score')
+colnames(df.dvir.blastn) <- c("Gene_ID","S", 'Scaffold_ID', 'Alignment_Length', 'Begin', 
+                              'End', 'E_val', 'Bit_score')
+colnames(df.dgri.blastn) <- c("Gene_ID","S", 'Scaffold_ID', 'Alignment_Length', 'Begin', 
+                              'End', 'E_val', 'Bit_score')
+
+write.table(df.dmoj.blastn, 'blastn_dmoj_converted.txt', col.names=T,row.names=F,quote=F, 
+            sep='\t')
+write.table(df.dvir.blastn, 'blastn_dvir_converted.txt', col.names=T,row.names=F,quote=F, 
+            sep='\t')
+write.table(df.dgri.blastn, 'blastn_dgri_converted.txt', col.names=T,row.names=F,quote=F, 
+            sep='\t')
+
 #calculate_cutoff(df$E_val)
 #dmoj_blastn_df<-df[df$E_val<2/140000000,]
 
-colnames(df) <- c('unique_ID', 'Scaffold_ID', 'Percent_Identity', 
-                           'Alignment_Length', 'Num_Mismathces', 'Num_Gap', 
-                           'mRNA_Begin', 'mRNA_End', 'Begin', 
-                           'End', 'E_val', 'Bit_score',"Gene_ID","S")
+# colnames(df) <- c('unique_ID', 'Scaffold_ID', 'Percent_Identity', 
+#                   'Alignment_Length', 'Num_Mismathces', 'Num_Gap', 
+#                   'mRNA_Begin', 'mRNA_End', 'Begin', 
+#                   'End', 'E_val', 'Bit_score',"Gene_ID","S")
